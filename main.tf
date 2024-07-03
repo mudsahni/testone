a899b030a621ae4fed84e49d8330e01fe771b5d1
Mudit Sahni <muditsahni@msn.com>
2024-07-03 14:12:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,12 +24,26 @@ resource "google_cloud_run_service" "demo_app" {
   }
 }
 
+resource "google_project_service" "run_api" {
+  project = var.project_id
+  service = "run.googleapis.com"
+}
+
+resource "google_project_service" "iam_api" {
+  project = var.project_id
+  service = "iam.googleapis.com"
+}
 resource "google_project_iam_member" "run_invoker" {
   project = var.project_id
   role    = "roles/run.invoker"
-  member  = "allUsers"
+  member  = "serviceAccount:service-${data.google_project.project.number}@serverless-robot-prod.iam.gserviceaccount.com"
+}
+
+data "google_project" "project" {
+  project_id = var.project_id
 }
 
 output "cloud_run_url" {
   value = google_cloud_run_service.demo_app.status[0].url
 }
+
